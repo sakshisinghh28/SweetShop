712eaabddc8f7313e5befe2db7cdfd3006874655
Sakshi Singh <sakshisingh2882@gmail.com>
2025-09-20 19:56:10 +0530

    Delete Backend directory

--- Backend/src/app/app.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-root',
-  template: `<div class="container"><router-outlet></router-outlet></div>`
-})
-export class AppComponent {}
